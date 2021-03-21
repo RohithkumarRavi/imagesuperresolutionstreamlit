61cf8964a0060f9ecdfb578e69c3405604596300
RohithkumarRavi <58527336+RohithkumarRavi@users.noreply.github.com>
2021-03-21 15:21:35 +0530

    Update imageresolution.py

--- imageresolution.py
+++ imageresolution.py
@@ -90,5 +90,7 @@ if uploaded_file is not None:
     st.write("")
     if st.button('Upscale Now'):
         st.write("upscaling...")
+        bicubic = image.resize((224,224))
         gen_image1 = resolve_single(generator, np.array(image)).numpy()
+        st.image(bicubic, caption='Normal upscaled image')
         st.image(gen_image1, caption='Upscaled Image')
